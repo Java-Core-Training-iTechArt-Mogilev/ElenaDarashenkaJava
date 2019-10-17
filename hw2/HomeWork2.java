8324e0a1be61c286129ee00cd0a9854d03758f71
Elena-Darashenka <Elena.Darashenka@coxautoinc.com>
2019-10-17 11:38:10 +0300

    HW2 - fixes

--- hw2/HomeWork2.java
+++ hw2/HomeWork2.java
@@ -15,23 +15,26 @@ public class HomeWork2 {
 
         System.out.println("Array with random values: " + Arrays.toString(randomValuesArray));
 
-        for (int numbers : randomValuesArray) {
-            if (numbers < minValue) {
-                minValue = numbers;
+        for (int number : randomValuesArray) {
+
+            if (number < minValue) {
+                minValue = number;
             }
-        }
 
-        for (int numbers : randomValuesArray) {
-            if (numbers > maxValue) {
-                maxValue = numbers;
+            if (number > maxValue) {
+                maxValue = number;
             }
+
+            averageValue = number + averageValue;
         }
 
-        for (int numbers : randomValuesArray) {
-            averageValue = numbers + averageValue;
-        }
+        System.out.println("Max value in array: " + maxValue);
+        System.out.println("Min value in array: " + minValue);
+        System.out.println("Average value in array: " + (averageValue / randomValuesArray.length));
 
-        for (int i = 0; i < randomValuesArray.length; i++) {
+        for (
+                int i = 0;
+                i < randomValuesArray.length; i++) {
             for (int j = i + 1; j < (randomValuesArray.length); j++) {
                 if (randomValuesArray[i] > randomValuesArray[j]) {
                     int t = randomValuesArray[i];
@@ -41,9 +44,6 @@ public class HomeWork2 {
             }
         }
 
-        System.out.println("Min value in array: " + minValue);
-        System.out.println("Max value in array: " + maxValue);
-        System.out.println("Average value in array: " + (averageValue / randomValuesArray.length));
         System.out.println("Bubble sorted array: " + Arrays.toString(randomValuesArray)
                 .replaceAll("[],\\[]", ""));
     }
